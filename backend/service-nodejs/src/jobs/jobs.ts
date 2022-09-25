35af975e0ee6cd8f154c46370351fb28c889294c
Debian Bear <debianbear@gmail.com>
2022-09-25 14:04:53 +0800

    update dockerfile

--- backend/service-nodejs/src/jobs/jobs.ts
+++ backend/service-nodejs/src/jobs/jobs.ts
@@ -1,13 +1,27 @@
 import { schedule } from "node-cron";
 import { CACHE_KEY_TASK_LOG_SKIP } from "../constants/constants";
 import { getCache } from "../lib/common/cache";
+import prisma from "../lib/common/prisma";
+
+const DB_PAGE_SIZE = 1000;
 
 const initJobs = () => {
   const sendNotification = schedule("*/5 * * * * *", async () => {
-    const d = await getCache(CACHE_KEY_TASK_LOG_SKIP);
+    try {
+      const start = Date.now();
+      console.log("[sendNotification] start:", start);
 
-    console.log(d);
-    console.log("sendNotification running:", Date.now());
+      const taskLogSkipStr = await getCache(CACHE_KEY_TASK_LOG_SKIP);
+      let taskLogSkip = taskLogSkipStr === "" ? 0 : parseInt(taskLogSkipStr);
+      await prisma.a_task_logs.findMany({
+        skip: taskLogSkip,
+        take: DB_PAGE_SIZE,
+      });
+      console.log(taskLogSkip);
+      console.log("[sendNotification] done, cost:", Date.now() - start);
+    } catch (error) {
+      console.trace(error);
+    }
   });
   sendNotification.start();
 };
